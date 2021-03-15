ba1fd8db9b3becd7fd18fb35dc8f96465ef9ef7b
MSabo <72773063+LongPatrol@users.noreply.github.com>
2021-03-14 21:48:02 -0400

    Axes are on the graph, but not sized well.

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -16,30 +16,25 @@ var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
 
 // Select body, append SVG area to it, and set the dimensions
 var svg = d3
-  .select("body")
+  .select("#scatter")
   .append("svg")
   .attr("height", svgHeight)
   .attr("width", svgWidth);
 
-// scale y to chart height
-var yScale = d3.scaleLinear()
-  .domain([0, d3.max(dataArray)])
-  .range([chartHeight, 0]);
 
-// scale x to chart width
-var xScale = d3.scaleBand()
-  .domain(dataCategories)
-  .range([0, chartWidth])
-  .padding(0.05);
 
-// create axes
-var yAxis = d3.axisLeft(yScale);
-var xAxis = d3.axisBottom(xScale);
+
+
+// // create axes
+// var yAxis = d3.axisLeft(yScale);
+// var xAxis = d3.axisBottom(xScale);
 
 // Append a group to the SVG area and shift ('translate') it to the right and down to adhere
 // to the margins set in the "chartMargin" object.
 var chartGroup = svg.append("g")
-  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+    .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+
 
 // Load data 
 d3.csv("/assets/data/data.csv").then(function(healthData) {
@@ -50,27 +45,46 @@ d3.csv("/assets/data/data.csv").then(function(healthData) {
   // Cast the value to a number for each piece 
   healthData.forEach(function(data) {
     data.poverty = +data.poverty;
-    data.povertyMoe = +data.povertyMoe;
+    //data.povertyMoe = +data.povertyMoe;
     data.obesity = +data.obesity;
     //additional things
   });
+  // scale x to chart width
+    var xScale = d3.scaleLinear()
+    .domain([0, d3.max(healthData, d => d.poverty)])
+    .range([chartHeight, 0]);
+  // scale y to chart height
+    var yScale = d3.scaleLinear()
+    .domain([0, d3.max(healthData, d => d.obesity)])
+    .range([chartHeight, 0]);
 
-//   var barSpacing = 10; // desired space between each bar
-//   var scaleY = 10; // 10x scale on rect height
+  // Create two new functions passing our scales in as arguments
+  // These will be used to create the chart's axes
+  var bottomAxis = d3.axisBottom(xScale);
+  var leftAxis = d3.axisLeft(yScale).ticks(10);
 
-//   // Create a 'barWidth' variable so that the bar chart spans the entire chartWidth.
-//   var barWidth = (chartWidth - (barSpacing * (healthData.length - 1))) / healthData.length;
+  // Append two SVG group elements to the chartGroup area,
+  // and create the bottom and left axes inside of them
+  chartGroup.append("g")
+    .call(leftAxis);
+
+  chartGroup.append("g")
+    .attr("transform", `translate(0, ${chartHeight})`)
+    .call(bottomAxis);
 
     // Create code to build the chart
-  chartGroup.selectAll("dot")
+  chartGroup.selectAll(".dot")
     .data(healthData)
     .enter()
     .append("circle")
-    .classed("scatter", true)
+    .classed("dot", true)
     .attr("cx", function (d) { return d.poverty; } )
     .attr("cy", function (d) { return d.obesity; } )
     .attr("r", 1.5)
     .style("fill", "#blue")
+    .attr("width", d => chartWidth -xScale(d.poverty))
+    .attr("height", d => chartHeight - yScale(d.obesity));
+
 }).catch(function(error) {
   console.log(error);
 });
